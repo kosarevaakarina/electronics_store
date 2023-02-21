2320300dd9e7b466b372b5fe1462be3d8aa38130
user <kosareva.karina2013@yandex.ru>
2023-02-21 22:56:28 +0300

    change utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Electronic_store:
     discount = 0.85
     all = []
@@ -9,10 +12,23 @@ class Electronic_store:
         :param price: цена за единицу товара
         :param amount: количество товара
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.amount = amount
         self.all.append(self)
+
+    @property
+    def name(self):
+        """Возвращает значения приватного атрибута"""
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """Проверяет, что при задании названия товара длина его не превышает 10 символов"""
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            raise Exception("Длина наименования товара превышает 10 символов.")
 
     def calculate_total_price(self):
         """ Возвращает общую стоимость конкретного товара в магазине """
@@ -23,6 +39,18 @@ class Electronic_store:
         self.price *= self.discount
         return self.price
 
+    @classmethod
+    def instantiate_from_csv(cls, file_csv):
+        """Считывает данные из csv-файла и создает экземпляры класса"""
+        with open(file_csv) as file:
+            reader = list(csv.reader(file))
+            item = []
+            for i in range(1, len(reader)):
+                item.append(Electronic_store(reader[i][0], reader[i][1], reader[i][2]))
+        return item
 
-
+    @staticmethod
+    def is_integer(num):
+        """Проверяет, является ли чисто целым"""
+        return int(num) == num
 
